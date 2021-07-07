a1f81c2d744b14d8c4d6e8efbfa6f38abf3b9c7d
Igor-voy <60691835+Igor-voy@users.noreply.github.com>
2021-07-07 03:41:32 +0300

    Update name.js

--- 3-4/lib/name.js
+++ 3-4/lib/name.js
@@ -4,5 +4,5 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var _default = 'D. Bukin';
+var _default = 'I. Voytenko';
 exports["default"] = _default;
